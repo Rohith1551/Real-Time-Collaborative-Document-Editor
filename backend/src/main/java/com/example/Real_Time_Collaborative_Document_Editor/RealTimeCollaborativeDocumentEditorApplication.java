f887a99d1342b709bf6c398d60e78bbea92a793e
Rohith R <rohith15022005@gmail.com>
2026-08-09 13:53:04 +0530

    added to repo

---
+++ backend/src/main/java/com/example/Real_Time_Collaborative_Document_Editor/RealTimeCollaborativeDocumentEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Real_Time_Collaborative_Document_Editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeCollaborativeDocumentEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeCollaborativeDocumentEditorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Real_Time_Collaborative_Document_Editor/RealTimeCollaborativeDocumentEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Real_Time_Collaborative_Document_Editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeCollaborativeDocumentEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
